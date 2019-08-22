bad0fa86f0599c414e00d74f8d8d024ffc894ce9
yurika sakae <yurika.sun@gmail.com>
2019-08-21 21:45:48 -0700

    test Controls button test added

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+
+import React from "react";
+import Controls from "./Controls";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("</Display", () => {
+  it("renders without crushing", () => {
+    render(<Controls />);
+  });
+  it("open and unlocked", () => {
+    const { getByText } = render(<Controls closed={false} locked={false} />);
+    const closeBtn = getByText(/close gate/i);
+    const lockBtn = getByText(/lock gate/i);
+    expect(closeBtn.disabled).toBeFalsy();
+    expect(lockBtn.disabled).toBeTruthy();
+  });
+});
